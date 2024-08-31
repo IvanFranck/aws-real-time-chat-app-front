15968da65daf8dae3ba2d66e57af028854a2d9df
NZIMA Ivan <nzimaivan007@gmail.com>
2024-08-31 16:15:14 +0100

    🚧 integrating sidebar

--- src/components/app/chats-feed.tsx
+++ src/components/app/chats-feed.tsx
@@ -3,7 +3,9 @@ import { ScrollArea } from "@radix-ui/react-scroll-area";
 export default function ChatsFeed() {
     return (
         <ScrollArea className="h-full">
-
+            <div>
+                
+            </div>
         </ScrollArea>
     )
 }
