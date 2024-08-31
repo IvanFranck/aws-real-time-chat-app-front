50b20af4179205ea7e4df7dcf06287ac27dcc47c
ivan nzima <nzimaivan007@gmail.com>
2024-08-31 03:23:55 +0100

    setting up project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-xl text-red-500'>hello</h1>
+      <Button>Click</Button>
+    </>
+  )
+}
+
+export default App
